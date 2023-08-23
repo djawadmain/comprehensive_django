4691ba524e3c98485ffed80be54f9cee4d482dfb
DjawadMain <djawad.main@gmail.com>
2023-08-23 10:18:53 +0330

    add rangemiddleware to settings

--- hellopycharm/settings.py
+++ hellopycharm/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'DjangoRangeMiddleware.middleware.RangesMiddleware'
 ]
 
 ROOT_URLCONF = 'hellopycharm.urls'
